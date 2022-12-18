cc4dde2cbc984a9491f6be63d87ca93deba7a460
evoLonation <1838940019@qq.com>
2022-12-18 11:14:59 +0800

    为LValue提供equals和hashCode方法

--- src/midcode/value/LValue.java
+++ src/midcode/value/LValue.java
@@ -1,5 +1,7 @@
 package midcode.value;
 
+import java.util.Objects;
+
 /**
  * 可以储存东西进去的
  */
@@ -16,5 +18,20 @@ public abstract class LValue implements RValue {
     @Override
     public String print() {
         return name;
+    }
+
+    // todo 注意，这种相等的比较只能保证在一个函数内是唯一的，不保证跨函数的唯一性
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof LValue){
+            return name.equals(((LValue) obj).name);
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
